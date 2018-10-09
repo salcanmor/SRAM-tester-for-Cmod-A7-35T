a927b44685c184a68ba0dfa3dcab03cc39f9d4df
Salvador Canas Moreno <cybero@hotmail.es>
2018-10-09 16:33:15 +0200

    Update top.v

--- top.v
+++ top.v
@@ -14,18 +14,36 @@ module top
     input wire clk,
     input wire reset,
     input wire data_in,
-    output wire data_out
-	// output wire [7:0] leds
-  );
+    output wire data_out,
+	 
+	 // to/from SRAM
+ output wire [18:0] ad,   
+inout wire [7:0] dio_a, 
+	 output wire we_n,
+	 output wire oe_n,
+	 output wire ce_a_n
+	 );
 
 wire [7:0] r_data_bus;
 wire [7:0] w_data_bus;
+wire [7:0] data_f2s_bus;
+wire [7:0] data_s2f_r_bus;
+wire [18:0] addr_bus;
 
+  wire button_wire;
+
+  debouncer debouncer_unit (
+    .clk(clk),     
+    .PB(reset), 
+    .PB_state(), 
+    .PB_down(), 
+    .PB_up(button_wire)
+  );
 
 
 uart uart_unit (
     .clk(clk), 
-    .reset(reset), 
+    .reset(button_wire), 
     .data_in(data_in), 
     .tx_start(tx_start), 
     .w_data(w_data_bus), 
@@ -39,19 +57,41 @@ uart uart_unit (
 
 
 
+// Instantiate the module
 checker checker_unit (
     .clk(clk), 
-    .reset(reset), 
+    .reset(button_wire), 
     .rx_done_tick(rx_done_tick), 
+    .tx_ready(tx_ready), 
     .r_data(r_data_bus), 
-    .w_data(w_data_bus),
-    .tx_start(tx_start),
-    .tx_ready(tx_ready)
-
+    .w_data(w_data_bus), 
+    .tx_start(tx_start), 
+    .mem(mem), 
+    .rw(rw), 
+    .addr(addr_bus), 
+    .data_s2f_r(data_s2f_r_bus), 
+    .data_f2s(data_f2s_bus)
     );
-
-
-//assign leds=w_data_bus;
-
+	 
+	 
+	 
+	 
+// Instantiate the module
+sram_ctrl sram_ctrl_unit (
+    .clk(clk), 
+    .reset(reset), 
+    .mem(mem), 
+    .rw(rw), 
+    .addr(addr_bus), 
+    .data_f2s(data_f2s_bus), 
+    .ready(ready), 
+    .data_s2f_r(data_s2f_r_bus), 
+    .data_s2f_ur(data_s2f_ur), 
+    .ad(ad), 
+    .we_n(we_n), 
+    .oe_n(oe_n), 
+    .dio_a(dio_a), 
+    .ce_a_n(ce_a_n)
+    );
 
 endmodule
